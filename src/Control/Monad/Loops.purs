e098e211d208947db98a8332eba776f5ee61eedc
Mario Lang <mlang@delysid.org>
2016-12-23 21:02:11 +0100

    Add {while,until}M{R,R'} for MonadRec.

--- src/Control/Monad/Loops.purs
+++ src/Control/Monad/Loops.purs
@@ -1,10 +1,15 @@
 module Control.Monad.Loops (
-  whileM, whileM', whileM_,
-  untilM, untilM', untilM_
+  whileM, whileM', whileM_, whileMR, whileMR',
+  untilM, untilM', untilM_, untilMR, untilMR'
 ) where
 
+import Control.Monad.Rec.Class (class MonadRec, Step(Done, Loop), tailRecM)
 import Data.Monoid (class Monoid, mempty)
-import Prelude (class Applicative, class Monad, Unit, bind, ifM, map, not, pure, unit, ($), (*>), (<>), (>>=))
+import Prelude ( class Applicative, class Monad, class Semigroup
+               , Unit
+               , append, bind, ifM, map, not, pure, unit
+               , ($), (*>), (<>), (<$>), (<<<), (>>=)
+               )
 
 whileM :: forall a m. Monad m => m Boolean -> m a -> m (Array a)
 whileM = whileM'
@@ -18,6 +23,14 @@ whileM' p f = ifM p
 whileM_ :: forall a m. Monad m => m Boolean -> m a -> m Unit
 whileM_ p f = ifM p (f *> whileM_ p f) (pure unit)
 
+whileMR :: forall a m. MonadRec m => m Boolean -> m a -> m (Array a)
+whileMR = whileMR'
+
+whileMR' :: forall a f m. (MonadRec m, Applicative f, Monoid (f a))
+         => m Boolean -> m a -> m (f a)
+whileMR' p f = tailRecM go mempty where
+  go xs = ifM p (f >>= pure <<< Loop <<< append xs <<< pure) (pure $ Done xs)
+
 untilM :: forall a m. Monad m => m a -> m Boolean -> m (Array a)
 untilM = untilM'
 
@@ -30,3 +43,12 @@ untilM' f p = do
 
 untilM_ :: forall a m. Monad m => m a -> m Boolean -> m Unit
 untilM_ f p = f *> whileM_ p f
+
+untilMR :: forall a m. MonadRec m => m a -> m Boolean -> m (Array a)
+untilMR = untilMR'
+
+untilMR' :: forall a f m. (MonadRec m, Applicative f, Semigroup (f a))
+         => m a -> m Boolean -> m (f a)
+untilMR' f p = f >>= tailRecM go <<< pure where
+  go xs = ifM p' (f >>= pure <<< Loop <<< append xs <<< pure) (pure $ Done xs)
+  p' = not <$> p
