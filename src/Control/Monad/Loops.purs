c3dd1b4a69b443dc7e96a37fcd7402f420cea656
Mario Lang <mlang@delysid.org>
2016-12-23 16:41:08 +0100

    Add whileM' and untilM'.

--- src/Control/Monad/Loops.purs
+++ src/Control/Monad/Loops.purs
@@ -1,20 +1,32 @@
-module Control.Monad.Loops (whileM, whileM_, untilM, untilM_) where
+module Control.Monad.Loops (
+  whileM, whileM', whileM_,
+  untilM, untilM', untilM_
+) where
 
-import Prelude
+import Data.Monoid (class Monoid, mempty)
+import Prelude (class Applicative, class Monad, Unit, bind, ifM, map, not, pure, unit, ($), (*>), (<>), (>>=))
 
 whileM :: forall a m. Monad m => m Boolean -> m a -> m (Array a)
-whileM p f = ifM p
-  (f >>= \ x -> whileM p f >>= \ xs -> pure $ [x] <> xs)
-  (pure [])
+whileM = whileM'
+
+whileM' :: forall a f m. (Monad m, Applicative f, Monoid (f a))
+        => m Boolean -> m a -> m (f a)
+whileM' p f = ifM p
+  (f >>= \ x -> whileM' p f >>= \ xs -> pure $ pure x <> xs)
+  (pure mempty)
 
 whileM_ :: forall a m. Monad m => m Boolean -> m a -> m Unit
 whileM_ p f = ifM p (f *> whileM_ p f) (pure unit)
 
 untilM :: forall a m. Monad m => m a -> m Boolean -> m (Array a)
-untilM f p = do
+untilM = untilM'
+
+untilM' :: forall a f m. (Monad m, Applicative f, Monoid (f a))
+        => m a -> m Boolean -> m (f a)
+untilM' f p = do
   x <- f
-  xs <- whileM (map not p) f
-  pure $ [x] <> xs
+  xs <- whileM' (map not p) f
+  pure $ pure x <> xs
 
 untilM_ :: forall a m. Monad m => m a -> m Boolean -> m Unit
 untilM_ f p = f *> whileM_ p f
