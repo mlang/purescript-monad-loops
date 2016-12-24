a1d32c4cd2a7bcf0a63555ae9e4b36c8fffc9813
Mario Lang <mlang@delysid.org>
2016-12-24 10:45:43 +0100

    Split MonadRec functions into their own module Control.Monad.Rec.Loops.

--- src/Control/Monad/Loops.purs
+++ src/Control/Monad/Loops.purs
@@ -1,15 +1,14 @@
 module Control.Monad.Loops (
-  whileM, whileM', whileM_, whileMR, whileMR', whileMR_,
-  untilM, untilM', untilM_, untilMR, untilMR', untilMR_
+  whileM, whileM', whileM_,
+  untilM, untilM', untilM_,
+  iterateWhile, iterateUntil, iterateUntilM
 ) where
 
-import Control.Monad.Rec.Class (class MonadRec, Step(Done, Loop), tailRecM)
-import Data.Functor (voidLeft)
 import Data.Monoid (class Monoid, mempty)
-import Prelude ( class Applicative, class Monad, class Semigroup
+import Prelude ( class Applicative, class Monad
                , Unit
-               , append, ifM, not, pure, unit
-               , (*>), (<$>), (<*>), (<<<), (>>=)
+               , append, const, ifM, not, pure, unit
+               , (*>), (<$>), (<<<), (>>=)
                )
 
 whileM :: forall a m. Monad m => m Boolean -> m a -> m (Array a)
@@ -24,16 +23,6 @@ whileM' p f = ifM p
 whileM_ :: forall a m. Monad m => m Boolean -> m a -> m Unit
 whileM_ p f = ifM p (f *> whileM_ p f) (pure unit)
 
-whileMR :: forall a m. MonadRec m => m Boolean -> m a -> m (Array a)
-whileMR = whileMR'
-
-whileMR' :: forall a f m. (MonadRec m, Applicative f, Monoid (f a))
-         => m Boolean -> m a -> m (f a)
-whileMR' p f = tailRecM (ifM p <$> collect f <*> done) mempty
-
-whileMR_ :: forall a m. MonadRec m => m Boolean -> m a -> m Unit
-whileMR_ p f = tailRecM (ifM p <$> voidLeft f <<< Loop <*> done) unit
-
 untilM :: forall a m. Monad m => m a -> m Boolean -> m (Array a)
 untilM = untilM'
 
@@ -44,21 +33,16 @@ untilM' f p = f >>= \ x -> whileM' (not <$> p) f >>= pure <<< append (pure x)
 untilM_ :: forall a m. Monad m => m a -> m Boolean -> m Unit
 untilM_ f p = f *> whileM_ p f
 
-untilMR :: forall a m. MonadRec m => m a -> m Boolean -> m (Array a)
-untilMR = untilMR'
+-- | Execute an action repeatedly until its result fails to satisfy a predicate,
+-- and return that result (discarding all others).
+iterateWhile :: forall a m. Monad m => (a -> Boolean) -> m a -> m a
+iterateWhile p = iterateUntil (not <<< p)
 
-untilMR' :: forall a f m. (MonadRec m, Applicative f, Semigroup (f a))
-         => m a -> m Boolean -> m (f a)
-untilMR' f p = f >>= tailRecM (ifM p <$> done <*> collect f) <<< pure
+-- | Yields the result of applying f until p holds.
+iterateUntilM :: forall a m. Monad m => (a -> Boolean) -> (a -> m a) -> a -> m a
+iterateUntilM p f v = if p v then pure v else f v >>= iterateUntilM p f
 
-untilMR_ :: forall a m. MonadRec m => m a -> m Boolean -> m Unit
-untilMR_ f p = f *> tailRecM (ifM p <$> done <*> voidLeft f <<< Loop) unit
-
--------------------------------------------------------------------------------
-
-collect :: forall a b f m. (MonadRec m, Applicative f, Semigroup (f a))
-        => m a -> f a -> m (Step (f a) b)
-collect f xs = f >>= pure <<< Loop <<< append xs <<< pure
-
-done :: forall a b m. MonadRec m => b -> m (Step a b)
-done = pure <<< Done
+-- | Execute an action repeatedly until its result satisfies a predicate,
+-- and return that result (discarding all others).
+iterateUntil :: forall a m. Monad m => (a -> Boolean) -> m a -> m a
+iterateUntil p x = x >>= iterateUntilM p (const x)
---
+++ src/Control/Monad/Rec/Loops.purs
@@ -0,0 +1,58 @@
+module Control.Monad.Rec.Loops (
+  whileM, whileM', whileM_,
+  untilM, untilM', untilM_,
+  iterateWhile, iterateUntil, iterateUntilM
+) where
+
+import Control.Monad.Rec.Class (class MonadRec, Step(Done, Loop), tailRecM)
+import Data.Functor (voidLeft)
+import Data.Monoid (class Monoid, mempty)
+import Prelude ( class Applicative, class Semigroup
+               , Unit
+               , append, const, ifM, not, pure, unit
+               , ($), (*>), (<$>), (<*>), (<<<), (>>=)
+               )
+
+whileM :: forall a m. MonadRec m => m Boolean -> m a -> m (Array a)
+whileM = whileM'
+
+whileM' :: forall a f m. (MonadRec m, Applicative f, Monoid (f a))
+         => m Boolean -> m a -> m (f a)
+whileM' p f = tailRecM (ifM p <$> collect f <*> done) mempty
+
+whileM_ :: forall a m. MonadRec m => m Boolean -> m a -> m Unit
+whileM_ p f = tailRecM (ifM p <$> voidLeft f <<< Loop <*> done) unit
+
+untilM :: forall a m. MonadRec m => m a -> m Boolean -> m (Array a)
+untilM = untilM'
+
+untilM' :: forall a f m. (MonadRec m, Applicative f, Semigroup (f a))
+         => m a -> m Boolean -> m (f a)
+untilM' f p = f >>= tailRecM (ifM p <$> done <*> collect f) <<< pure
+
+untilM_ :: forall a m. MonadRec m => m a -> m Boolean -> m Unit
+untilM_ f p = f *> tailRecM (ifM p <$> done <*> voidLeft f <<< Loop) unit
+
+-- | Execute an action repeatedly until its result fails to satisfy a predicate,
+-- and return that result (discarding all others).
+iterateWhile :: forall a m. MonadRec m => (a -> Boolean) -> m a -> m a
+iterateWhile p = iterateUntil $ not <<< p
+
+-- | Yields the result of applying f until p holds.
+iterateUntilM :: forall a m. MonadRec m
+              => (a -> Boolean) -> (a -> m a) -> a -> m a
+iterateUntilM p f = tailRecM (\ v -> if p v then pure $ Done v else Loop <$> f v)
+
+-- | Execute an action repeatedly until its result satisfies a predicate,
+-- and return that result (discarding all others).
+iterateUntil :: forall a m. MonadRec m => (a -> Boolean) -> m a -> m a
+iterateUntil p x = x >>= iterateUntilM p (const x)
+
+-------------------------------------------------------------------------------
+
+collect :: forall a b f m. (MonadRec m, Applicative f, Semigroup (f a))
+        => m a -> f a -> m (Step (f a) b)
+collect f xs = f >>= pure <<< Loop <<< append xs <<< pure
+
+done :: forall a b m. MonadRec m => b -> m (Step a b)
+done = pure <<< Done
